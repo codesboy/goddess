5e87b9079126ef2123f1093f6b6a54d272502827
codesboy <565195693@qq.com>
2018-03-28 17:57:53 +0800

    update

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,52 +3,28 @@
 const app = getApp()
 
 Page({
-  data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
+    data: {
+        codes:'',
+        tpl:'大坏蛋撒旦撒谎就dsasd'
+    },
+
+    onShow: function () {
+        var that = this;
         this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
+            codes:''
         })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
+        var n=0;
+        clearInterval(timer)
+        var timer = setInterval(()=>{
+            this.setData({
+                codes: this.data.tpl.substring(0, n)
+            })
+            n++;
+            console.log(this.data.codes)
+        },100)
+    },
+
+    print(){
+
     }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  }
 })
